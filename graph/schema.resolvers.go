ff1cc43ec244b713bb25eae02d0b481e38bd40af
Danylo Lubenets <danylo.lubenets@pantheon.io>
2021-08-19 17:55:27 +0300

    optimized user retrieval

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -11,9 +11,9 @@ import (
 
 func (r *mutationResolver) CreateTodo(ctx context.Context, input NewTodo) (*Todo, error) {
 	todo := &Todo{
-		Text: input.Text,
-		ID:   fmt.Sprintf("T%d", rand.Int()),
-		User: &User{ID: input.UserID, Name: "user " + input.UserID},
+		Text:   input.Text,
+		ID:     fmt.Sprintf("T%d", rand.Int()),
+		UserID: input.UserID,
 	}
 	r.todos = append(r.todos, todo)
 	return todo, nil
@@ -23,11 +23,19 @@ func (r *queryResolver) Todos(ctx context.Context) ([]*Todo, error) {
 	return r.todos, nil
 }
 
+func (r *todoResolver) User(ctx context.Context, obj *Todo) (*User, error) {
+	return &User{ID: obj.UserID, Name: "user " + obj.UserID}, nil
+}
+
 // Mutation returns MutationResolver implementation.
 func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
 
 // Query returns QueryResolver implementation.
 func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
 
+// Todo returns TodoResolver implementation.
+func (r *Resolver) Todo() TodoResolver { return &todoResolver{r} }
+
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+type todoResolver struct{ *Resolver }
